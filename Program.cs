06074910d867f265a4eb1c779a70b721a259af63
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-11-02 14:35:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Topic_9___Making_A_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student person = new Student("Robert", "Ross");
+            Console.WriteLine(person.FirstName);
+            //Changing First Name Value
+            person.FirstName = "Bobert";
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine("");
+            Console.WriteLine(person.LastName);
+            //Changing Last Name Value
+            person.LastName = "Boss";
+            Console.WriteLine(person.LastName);
+            Console.WriteLine("");
+            Console.WriteLine(person.StudentNumber);
+            Console.WriteLine("");
+            // Printing Class Manually
+            Console.WriteLine(person.FirstName + " " + person.LastName + " " + person.StudentNumber);
+            Console.WriteLine("");
+            //Simplified Class Print
+            Console.WriteLine(person);
+            Console.WriteLine("");
+            //Printing Email
+            Console.WriteLine(person.Email);
+            Console.WriteLine("");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("");
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student("Bob", "Ross"));
+
+            Console.WriteLine("How many students are in your class?");
+            Console.WriteLine("Please Enter a number..");
+            int classNumber = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < classNumber; i++)
+            {
+                Console.WriteLine("Please enter the student's first name..");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("Please enter the student's last name..;");
+                string lastname = Console.ReadLine();
+                students.Add(new Student(firstname, lastname));
+                Console.WriteLine(students[i + 1]);
+                Console.WriteLine("Thank You.");
+                
+                Console.WriteLine("");
+
+            }
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i]);
+        
+            }
+            Console.WriteLine("There are " + students.Count + " students in your class.");
+        }
+        
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topic_9___Making_A_Class
+{
+    class Student
+    {
+        private static Random generator = new Random();
+        //Defining Variables
+        private string _firstname;
+        private string _lastname;
+        private int _studentNumber;
+        private string _email;
+
+
+        public Student(string firstName, string lastName)
+        {
+            //Instance variables, fields initialized here
+
+            _firstname = firstName;
+            _lastname = lastName;
+            _studentNumber = generator.Next(100,1000);
+            GenerateEmail();
+        }
+        //Controlling What Can Be Controlled In The Main Form
+        public string FirstName
+        {
+            get
+            {
+                return _firstname;
+            }
+            set
+            {
+                _firstname = value;
+                //Change Email everytime names change
+                GenerateEmail();
+            }
+
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+                //Change Email everytime names change
+                GenerateEmail();
+            }
+        }
+        public int StudentNumber
+        {
+                get
+            {
+                return _studentNumber;
+            }
+        }
+        //Allowing you to call _email
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            
+        }
+
+        // Simplify Console.WriteLine(class);
+        public override string ToString()
+        {
+            return _firstname + " " + _lastname;
+        }
+        
+        //Making a method that generates _email
+        private void GenerateEmail()
+        {
+            //First 3 letters of First Name, First 3 Letters of the Last Name, Student Number, @ICS4U.ca
+            _email = _firstname.Substring(0, 3) + _lastname.Substring(0, 3) + _studentNumber + "@ICS4U.ca";
+
+
+        }
+
+
+
+
+    }
+}
